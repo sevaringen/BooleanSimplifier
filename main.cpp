190951e8a93e9d6a468eb8927ec48d249c1a428c
sevaringen <59581205+sevaringen@users.noreply.github.com>
2021-02-28 19:07:49 +0000

    Update main.cpp allowing for all inputs
    All user inputs have been included

--- main.cpp
+++ main.cpp
@@ -1,28 +1,58 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
 	std::string tmpInputs;
-	std::int intInputs;
+	int intProducts;
+    int intInputs;
 	std::string strInputs;
-	std::bool isValid;
+	bool isValid;
 
-	std::cout << "Enter the number of inputs";
+	std::cout << "Enter the number of products (number of sumations)" << std::endl;
 	while (!isValid) 
 	{
-		tmpInputs << cin;
+		tmpInputs << std::cin;
 		if (isNumeric(tmpInputs)) 
 		{
 			if (tmpInputs < 5 && tmpInputs > 0)
 			{
-				isValid = true
+				intProducts = stoi(tmpInputs)
+                isValid = true;
 				std::cout << "Input is valid";
 			}	
-		}
+		} else {
+            std::cout "Enter a positive integer";
+        }
 	}
+
+    std::cout << "Enter the number of inputs (number of gates)" << std::endl;
+    while (isValid) 
+	{
+		tmpInputs << std::cin;
+		if (isNumeric(tmpInputs)) 
+		{
+			if (tmpInputs < 5 && tmpInputs > 0)
+			{
+				intInputs = stoi(tmpInputs)
+                isValid = false;
+				std::cout << "Input is valid";
+			}	
+		} else {
+            std::cout "Enter a positive integer";
+        }
+	}
+
+    char* products;
+    for (int j = 0; j < intProducts; j++)
+    {
+        cout << "Enter another product." << << std::end;
+        products[j] = std::cin;
+    }
+
 }
 
-bool isNumeric(string checkString)
+bool isNumeric(std::string checkString)
 {
 	for (int i = 0; i < checkString.length(); i++)
 	{
